f198ff91351419b2023a7b2106f73c7ea2a4a2c6
Nathan Moore <ncmoore1986@gmail.com>
2017-10-19 14:04:24 -0700

    we now have a usable list of words

--- trigrams.py
+++ trigrams.py
@@ -3,9 +3,15 @@
 
 def main(path, n):
     """Make a docstring."""
+    import string
     with open(path) as book_file:
         book = book_file.read()
-        return book
+        raw_word_list = book.split()
+        word_list = []
+        for word in raw_word_list:
+            word_list.append(word.strip(string.punctuation))
+
+        print(word_list)
 
 
 main('test_book.txt', 1)
