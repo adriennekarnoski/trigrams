c4891fcd62dca050e7b13413bcb673735a07e9e0
Nathan Moore <ncmoore1986@gmail.com>
2017-10-19 14:36:40 -0700

    added generate_trigrams function

--- trigrams.py
+++ trigrams.py
@@ -3,15 +3,33 @@
 
 def main(path, n):
     """Make a docstring."""
+    trigrams = generate_trigrams(book_into_list(path))
+
+
+def book_into_list(path_to_book):
+    """Break the book.txt file into a list of words without punctuation."""
     import string
-    with open(path) as book_file:
+
+    with open(path_to_book) as book_file:
         book = book_file.read()
         raw_word_list = book.split()
         word_list = []
         for word in raw_word_list:
             word_list.append(word.strip(string.punctuation))
 
-        print(word_list)
+    return word_list
 
+
+def generate_trigrams(words):
+    """Take in a list of words and return a dictionary of trigrams."""
+    generated_trigrams = {}
+    for idx, word in enumerate(words):
+        pair = word[idx] + word[idx + 1]
+        if pair in generated_trigrams:
+            generated_trigrams[pair].append(word[idx + 2])
+        else:
+            generated_trigrams[pair] = [word[idx + 2]]
+
+    return generated_trigrams
 
 main('test_book.txt', 1)
